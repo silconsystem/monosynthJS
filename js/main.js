6e7dd2a015fdf14de07425b85528865f8f2ae3d8
silconsystem <silcon.art.system@gmail.com>
2019-03-13 18:25:37 +0100

    layout work

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var waveSelect = document.getElementById('waveselect');
+
+waveSelect.oninput = function() {
+  document.getElementById('selected-waveform').innerHTML = waveSelect.value;
+  waveSelect.value = '';
+}
+
