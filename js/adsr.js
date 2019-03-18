4b9c12e4e22eca356129a619ce3c260fef4a7a8f
silconsystem <silcon.art.system@gmail.com>
2019-03-18 19:50:07 +0100

    started ADSR module

---
+++ js/adsr.js
@@ -0,0 +1,12 @@
+/** 			adsr.js
+	**/
+// control elements
+var adsrOneAttack 	= document.getElementById('adsr-1-attack');
+var adsrOneDecay 	= document.getElementById('adsr-1-decay');
+var adsrOneSustain 	= document.getElementById('adsr-1-sustain');
+var adsrOneRelease 	= document.getElementById('adsr-1-release');
+
+var adsrTwoAttack 	= document.getElementById('adsr-2-attack');
+var adsrTwoDecay 	= document.getElementById('adsr-2-decay');
+var adsrTwoSustain 	= document.getElementById('adsr-2-sustain');
+var adsrTwoRelease 	= document.getElementById('adsr-2-release');
