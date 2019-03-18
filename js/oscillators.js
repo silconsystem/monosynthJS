cb46f1df32486975a41fe646385e44c4119b63af
silconsystem <silcon.art.system@gmail.com>
2019-03-18 17:31:27 +0100

    added reset for the oscillators, polished up the code

--- js/oscillators.js
+++ js/oscillators.js
@@ -2,8 +2,124 @@
 	**/
 /**				 Globals				**/
 // range input for osc one
-var oscOneFreqInput = document.getElementById('osc-1-freq-input');
-var oscOneGainInput = document.getElementById('osc-1-gain-input');
+var oscOneReset = document.getElementById('osc-1-reset');
+var oscTwoReset = document.getElementById('osc-2-reset');
+
+// 	waveform selectors
+var waveSelectOne = document.getElementById('osc-1-waveselect');
+var waveSelectTwo = document.getElementById('osc-2-waveselect');
+
+var wavOneDisplay = document.getElementById('osc-1-selected-waveform');
+var wavTwoDisplay = document.getElementById('osc-2-selected-waveform');
+
+// flags, counters and get&set values
+var waveTypeOne = 'sine';
+var waveTypeTwo = 'sine'; 
+
+// reset the input selectors
+waveSelectOne.onchange = function() {
+	wavOneDisplay.innerHTML = waveSelectOne.value;
+	waveTypeOne = waveSelectOne.value;
+	waveSelectOne.value = '';
+	console.log("waveform-1: " + waveTypeOne);
+}
+waveSelectTwo.onchange = function() {
+	wavTwoDisplay.innerHTML = waveSelectTwo.value;
+	waveTypeTwo = waveSelectTwo.value;
+	waveSelectTwo.value = '';
+	console.log("waveform-2: " + waveTypeTwo);
+}
+// frequency and gain sliders
+var oscOneFreq = document.getElementById('osc-1-freq-input');
+var oscTwoFreq = document.getElementById('osc-2-freq-input');
+
+var noteOne = document.getElementById('note-1-value');
+var noteTwo = document.getElementById('note-2-value');
+
+var oscOneGain = document.getElementById('osc-1-gain-input');
+var oscTwoGain = document.getElementById('osc-2-gain-input');
+
+var gainOne = document.getElementById('osc-1-gain');
+var gainTwo = document.getElementById('osc-2-gain');
+
+var hertzOne = document.getElementById('osc-1-freq')
+var hzSrcnOne =  document.getElementById('freq-1-value');
+var hertzTwo = document.getElementById('osc-2-freq');
+var hzSrcnTwo = document.getElementById('freq-2-value');
+
+var frequencyOne,
+	frequencyTwo,
+	gainOneVal,
+	gainTwoVal;
+
+oscOneFreq.oninput = function() {
+	hertzOne.innerHTML = stdTuningFreq[this.value];
+	hzSrcnOne.innerHTML = stdTuningFreq[this.value];
+	noteOne.innerHTML = stdTuningNotes[this.value];
+	frequencyOne = stdTuningFreq[this.value];
+	console.log(frequencyOne);
+}
+oscOneFreq.oninput();
+oscTwoFreq.oninput = function() {
+	hertzTwo.innerHTML = stdTuningFreq[this.value];
+	hzSrcnTwo.innerHTML = stdTuningFreq[this.value];
+	noteTwo.innerHTML = stdTuningNotes[this.value];
+	frequencyTwo = stdTuningFreq[this.value];
+	console.log(frequencyTwo);
+}
+oscTwoFreq.oninput();
+
+oscOneGain.oninput = function() {
+	gainOne.innerHTML = oscOneGain.value;
+	gainOneVal = oscOneGain.value;
+	console.log(gainOneVal);
+}
+oscOneGain.oninput();
+oscTwoGain.oninput = function() {
+	gainTwo.innerHTML = oscTwoGain.value;
+	gainTwoVal = oscTwoGain.value;
+	console.log(gainTwoVal);
+}
+oscTwoGain.oninput();
+
+oscOneReset.onclick = function() {
+	oscOneFreq.value = 0;
+	hertzOne.innerHTML = stdTuningFreq[oscOneFreq.value];
+	hzSrcnOne.innerHTML = stdTuningFreq[oscOneFreq.value];
+	noteOne.innerHTML = stdTuningNotes[oscOneFreq.value];
+	oscOneGain.value = 0.5;
+	gainOne.innerHTML = oscOneGain.value;
+	waveSelectOne.value = '';
+	wavOneDisplay.innerHTML = 'sine';
+
+	frequencyOne = stdTuningFreq[oscOneFreq.value];
+	gainOneVal = 0.5;
+	waveTypeOne = 'sine';
+	console.log("freq: " + frequencyOne);
+	console.log("gain: " + gainOneVal);
+	console.log("wave: " + waveTypeOne);
+}
+
+oscTwoReset.onclick = function() {
+	oscTwoFreq.value = 0;
+	hertzTwo.innerHTML = stdTuningFreq[oscTwoFreq.value];
+	hzSrcnTwo.innerHTML = stdTuningFreq[oscTwoFreq.value];
+	noteTwo.innerHTML = stdTuningNotes[oscTwoFreq.value];
+	oscTwoGain.value = 0.5;
+	gainTwo.innerHTML = oscTwoGain.value;
+	waveSelectTwo.value = '';
+	wavTwoDisplay.innerHTML = 'sine';
+
+	frequencyTwo = stdTuningFreq[oscTwoFreq.value];
+	gainTwoVal = 0.5;
+	waveTypeTwo = 'sine';
+	console.log("freq: " + frequencyTwo);
+	console.log("gain: " + gainTwoVal);
+	console.log("wave: " + waveTypeTwo);
+}
+
+wavOneDisplay.innerHTML = waveTypeOne;
+wavTwoDisplay.innerHTML = waveTypeTwo;
 
 // the two oscillators
 var oscOne = new Tone.Oscillator({
